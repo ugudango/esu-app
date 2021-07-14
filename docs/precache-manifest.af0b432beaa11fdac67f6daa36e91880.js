0a3d8e447f1b91765e1ab13863e6a196fa18a32b
Gârleanu Alexandru-Ștefan <redmatterfriends@gmail.com>
2021-07-14 18:59:24 +0300

    Github Pages update

---
+++ docs/precache-manifest.af0b432beaa11fdac67f6daa36e91880.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be25b4b1ada160f4b412",
+    "url": "/css/app.d6ed2d4a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "05e153764bd4c31cf0ba8d886e0cb9d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdccab027fafc252018a",
+    "url": "/js/about.ff32cdf2.js"
+  },
+  {
+    "revision": "be25b4b1ada160f4b412",
+    "url": "/js/app.2e79f111.js"
+  },
+  {
+    "revision": "7ec1672206beae32b41c",
+    "url": "/js/chunk-vendors.f72d9b9a.js"
+  },
+  {
+    "revision": "3f90fa6ebc94c404ffa2eb31cdeffc41",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
