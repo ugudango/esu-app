1cd5dd623b7b235298aed33368ad666ece2d73f5
Gârleanu Alexandru-Ștefan <redmatterfriends@gmail.com>
2021-07-16 18:06:39 +0300

    Added some cards

--- src/components/GroupCard.vue
+++ src/components/GroupCard.vue
@@ -1,10 +1,6 @@
 <template>
 <!--eslint-disable max-len-->
-  <div class="home">
-    <div
-      class="container grid grid-cols-2 gap-4 p-4 bg-gray-400"
-    >
-      <div class="bg-gray-700 rounded-md flex flex-col items-center">
+  <div class="bg-gray-700 rounded-md flex flex-col items-center">
         <svg class="mt-4"
           color="white"
           style="width:48px;height:48px"
@@ -12,21 +8,12 @@
           <path fill="currentColor" d="M11.5,20L16.36,10.27H13V4L8,13.73H11.5V20M12,2C14.75,2 17.1,3 19.05,4.95C21,6.9 22,9.25 22,12C22,14.75 21,17.1 19.05,19.05C17.1,21 14.75,22 12,22C9.25,22 6.9,21 4.95,19.05C3,17.1 2,14.75 2,12C2,9.25 3,6.9 4.95,4.95C6.9,3 9.25,2 12,2Z" />
         </svg>
       </div>
-      <div class="bg-gray-700">1</div>
-      <div class="bg-gray-700">1</div>
-      <div class="bg-gray-700">1</div>
-      <div class="bg-gray-700">1</div>
-    </div>
-  </div>
 <!--eslint-enable max-len-->
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent } from 'vue';
 
-@Options({
-  components: {
-  },
-})
-export default class Home extends Vue {}
+export default defineComponent({
+});
 </script>
